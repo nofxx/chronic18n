09ce883c2e19449c7afc382355add745a15ea6a2
Christian Sousa <neocsr@gmail.com>
2011-01-16 23:27:48 +0900

    Added some tests for Spanish.

--- spec/chronic18n/es_spec.rb
+++ spec/chronic18n/es_spec.rb
@@ -26,5 +26,21 @@ describe "Spanish" do
     Chronic18n.parse("mayo 3", :es).month.should eql(5)
   end
 
+  it "should parse monday" do
+    Chronic18n.parse("mar", :es).month.should eql(3)
+  end
+
+  it "should parse month" do
+    Chronic18n.parse("may 3", :es).month.should eql(5)
+  end
+  
+  it "should parse next month" do
+    Chronic18n.parse("mes que viene", :es).month.should eql((Time.now.month + 1 % 12))
+  end
+  
+  it "should parse today" do
+    Chronic18n.parse("hoy a las 6pm", :es).day.should eql((Time.now.day))
+  end
+
 
 end
