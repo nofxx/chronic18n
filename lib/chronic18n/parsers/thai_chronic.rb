d2f5876d6f6e689e4a652d3a7f397c0cbdbcd1eb
TimNew <timnew@users.noreply.github.com>
2017-08-18 14:25:46 +1000

    Update thai_chronic.rb

--- lib/chronic18n/parsers/thai_chronic.rb
+++ lib/chronic18n/parsers/thai_chronic.rb
@@ -1,7 +1,7 @@
 module Chronic18n
   class ThaiChronic
     def self.parse(txt, options = {})
-      date = Chronic.parse(txt, options)
+      date = Chronic.parse(txt, options) # 2017 / 17 / 2560 / 60 is the same year
       if date
         current_year = Time.now.year # 2560 -> 2017 
         # incase it's short year
@@ -9,7 +9,7 @@ module Chronic18n
           date = Date.new(date.year + 500, date.month, date.day) 
         end
 
-        if date.year - current_year > 500 # 2060
+        if date.year - current_year > 500 # 2560
           date = Date.new(date.year - 543, date.month, date.day)
         end
       end
